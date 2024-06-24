948229885dca0baa6b52a301de5ec8cd58ed2feb
nik <nik@parkellipsen.de>
2024-06-24 13:25:10 +0200

    progress

--- web/index.js
+++ web/index.js
@@ -6,15 +6,15 @@ var div = document.getElementById("boo")
 var staves = {};
 
 // desired functions:
-// - name staves
+// - [x] name staves
 // - position staves
 // - group staves, i.e. put a color background
 // - [x] stave history, between 1 and 4 notes?
-// - dynamic mark on stave
-// - clef for stave
+// - [x] dynamic mark on stave
+// - [x] clef for stave
+//   - transpose notes according to clef 
 // - [x] color "active" note
-// - names for staves
-// - cute additions 
+// - cute additions (emojis for playing style ?)
 
 function render(name) {
     // clear score first ... this is not very efficient I suppose,    
@@ -23,42 +23,75 @@ function render(name) {
     var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
     
     var max_len = 0;
+    var spacing = 60;
+
+
     
     // Configure the rendering context.    
     var context = renderer.getContext();
-    context.setFont("Mononoki", 10, "").setBackgroundFillStyle("#eed");
+    context.setFont("mononoki", 10, "").setBackgroundFillStyle("#eed");
 
     var v_idx = 0;
     
-    for (const [name, notes] of Object.entries(staves)) {
-	let vis_len = 40 + notes.length * 30;
+    for (const [name, stave_props] of Object.entries(staves)) {
+	let vis_len = 40 + stave_props.notes.length * spacing;
 
 	if (vis_len > max_len) {
 	    renderer.resize(vis_len, 500);
 	    max_len = vis_len;
 	}
-	
+	 
 	// Create a stave of dynamic width at position 10, 40 on the canvas.
-	var stave = new VF.Stave(10, 40 + (100 * v_idx), vis_len);
+	var stave = new VF.Stave(10, spacing + (100 * v_idx), vis_len);
 			
 	v_idx = v_idx + 1;
 	
 	// Add a clef and time signature.
-	stave.addClef("treble");
+	stave.addClef(stave_props.clef).setContext(context).draw();
+			
+	var stave_name = new VF.TextNote({
+            text: name,
+            font: {
+		family: "Mononoki",
+		size: 12,
+		weight: "bold"
+            },
+            duration: 'w'               
+	})
+	    .setLine(2)
+	    .setStave(stave)
+	    .setJustification(VF.TextNote.Justification.LEFT);
 	
-	var voice = new VF.Voice({num_beats: notes.length,  beat_value: 1}).setStrict(false);
+	var dyn = new VF.TextNote({
+            glyph: stave_props.dyn,
+            font: {
+		family: "Mononoki",
+		size: 12,
+		weight: "bold"
+            },
+            duration: 'w'               
+	})
+	    .setLine(2)
+	    .setStave(stave)
+	    .setJustification(VF.TextNote.Justification.LEFT);
+	
 	
 	// Connect it to the rendering context and draw!
 	stave.setContext(context).draw();
 	
 	// Create a voice in 4/4 and add above notes
-
-	voice.addTickables(notes)
-
-	var formatter = new VF.Formatter().joinVoices([voice]).format([voice], notes.length * 30);
+	var voice = new VF.Voice({num_beats: stave_props.notes.length,  beat_value: 1}).setStrict(false);
+	var decorations = new VF.Voice({num_beats: 3,  beat_value: 1}).setStrict(false);
+	
+	voice.addTickables(stave_props.notes);
+	decorations.addTickables([stave_name, dyn]);
+	
+	var formatter = new VF.Formatter().joinVoices([voice,decorations]).format([voice,decorations], stave_props.notes.length * spacing);
 
 	// Render voice
 	voice.draw(context, stave);
+	stave_name.setContext(context).draw();
+	dyn.setContext(context).draw();
     }       
 }
 
@@ -73,29 +106,54 @@ oscPort.on("message", function (msg) {
 
     switch(msg.address) {
 	
-    case "/stave/dyn": {
+    case "/voice/dyn": {
 	var stave = msg.args[0].value;
-	var dyn = msg.args[1].value;
+	var dyn = msg.args[1].value;	
+
+	staves[stave].dyn = dyn;
+
+	break;
     }
-    case "/note/add": {
+    case "/voice/clef": {
+	var stave = msg.args[0].value;
+	var clef = msg.args[1].value;
+
+	staves[stave].clef = clef;
+
+	break;
+    }
+    case "/voice/note/add": {
 	var stave = msg.args[0].value;
 	var note = msg.args[1].value;
 	var dur = msg.args[2].value;
 	
 	if (staves[stave] === undefined) {
-	    staves[stave] = [];
+	    staves[stave] = {};
+	    staves[stave].notes = [];
+	}
+
+	if (staves[stave].notes === undefined) {
+	    staves[stave].notes = [];
+	}
+
+	if (staves[stave].clef === undefined) {
+	    staves[stave].clef = "treble";
+	}
+
+	if (staves[stave].dyn === undefined) {
+	    staves[stave].dyn = "p";
 	}
      
-	staves[stave].push(new VF.StaveNote({ keys: [note], duration: dur }));
+	staves[stave].notes.push(new VF.StaveNote({ keys: [note], duration: dur }));
 	
-	if (staves[stave].length > 3) {
-	    staves[stave].shift();
+	if (staves[stave].notes.length > 3) {
+	    staves[stave].notes.shift();
 	}
 
 	// set the current note to a different color ...
-	staves[stave][0].setStyle({ fillStyle: "#000000", strokeStyle: "#000000" })
-	staves[stave][1].setStyle({ fillStyle: "#FF0000", strokeStyle: "#FF0000" })
-	staves[stave][2].setStyle({ fillStyle: "#000000", strokeStyle: "#000000" })
+	staves[stave].notes[0].setStyle({ fillStyle: "#000000", strokeStyle: "#000000" })
+	staves[stave].notes[1].setStyle({ fillStyle: "#FF0000", strokeStyle: "#FF0000" })
+	staves[stave].notes[2].setStyle({ fillStyle: "#000000", strokeStyle: "#000000" })
 		
 	render();
 
