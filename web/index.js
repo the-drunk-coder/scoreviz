a3d73f7c7588dbaf881eb0bf247f73b6158c1cbd
nik <nik@parkellipsen.de>
2024-08-10 21:07:36 +0200

    towards multiple bars

--- web/index.js
+++ web/index.js
@@ -1,8 +1,6 @@
 // get vexflow API
 const VF = Vex.Flow;
 
-var div = document.getElementById("boo")
-
 var staves = {};
 var textfields = {};
 
@@ -23,118 +21,107 @@ var textfields = {};
 // - [x] clef for stave
 // - [x] color "active" note
 
+function notes_to_measures(notes, lower, upper) {
+    let inner_notes = notes.slice();
+
+    let measures = [];
+    let measure = [];
+
+    let ticks_to_fill_per_bar = upper * (16384 / lower);
+
+    let num_notes = inner_notes.length;
+    
+    for (var i = 0; i < num_notes; i++) {
+	
+	let note = inner_notes.shift();
+
+	if (note.intrinsicTicks <= ticks_to_fill_per_bar) {	    
+	    measure.push(note);
+	    ticks_to_fill_per_bar -= note.intrinsicTicks
+	} else {
+	    // reset current measure
+	    measures.push(measure);
+	    measure = [];
+	    ticks_to_fill_per_bar = upper * (16384 / lower);
+	    // add note 
+	    measure.push(note);
+	    ticks_to_fill_per_bar -= note.intrinsicTicks
+	}
+    }
+    
+    if (ticks_to_fill_per_bar > 0) {
+	// add rests ...
+    }
+
+    // last measure
+    measures.push(measure);
+    
+    return measures;
+}
+
+var div = document.getElementById("boo");
+
 function render() {
-
-    // clear score first ... this is not very efficient I suppose,    
+    
+    // clear score first ... this is not very efficient I suppose, 
     div.innerHTML = '';
-    
-    var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-    
-    var max_len = 0;
-    var spacing = 60;
-    
-    // Configure the rendering context.    
-    var context = renderer.getContext();
-    // this has no effect ?
-    
-    context.setFont("mononoki", 10, "");
-    
-    var v_idx = 0;
 
+    const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+    renderer.resize(1200, 1200);
+    
+    const context = renderer.getContext();
+
+    // get svg for stuff like background color ...
     let svg = document.getElementsByTagName('svg')[0];
-
+    
     // render staves 
     for (const [name, stave_props] of Object.entries(staves)) {
 
-	let vis_len = stave_props.x + stave_props.notes.length * spacing;
+	let measures = notes_to_measures(
+	    stave_props.notes,
+	    stave_props.timesignature.upper,
+	    stave_props.timesignature.lower
+	);
 
-	renderer.resize(1000, 1000);
-	 
-	// Create a stave of dynamic width at position 10, 40 on the canvas.
-	var stave = new VF.Stave(stave_props.x, stave_props.y, vis_len);
-	
-	// create a background color if defined, as an SVG rectangle
-	// (vexflow doesn't have facilities for this by itself)
+	// estimate width
+	let rect_width = stave_props.x + (measures.length * 240) + 80;
+
+	// stave background color
 	if (stave_props.bgcolor) {
 	    var svgns = "http://www.w3.org/2000/svg";
 	    var rect = document.createElementNS(svgns, 'rect');
 	    rect.setAttributeNS(null, 'x', stave_props.x);
 	    rect.setAttributeNS(null, 'y', stave_props.y + 12);
 	    rect.setAttributeNS(null, 'height', 90);
-	    rect.setAttributeNS(null, 'width', vis_len);
+	    rect.setAttributeNS(null, 'width', rect_width);
 	    rect.setAttributeNS(null, 'fill', '#' + stave_props.bgcolor);
 	    svg.appendChild(rect);
 	}
 	
-	if (stave_props.label) {
-	    var label = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-	    label.setAttributeNS(null, 'x', stave_props.x + vis_len + 12);
-	    label.setAttributeNS(null, 'y', stave_props.y + 70);
-	    label.setAttributeNS(null, 'fill', '#000');
-	    label.setAttributeNS(null, 'font-size', '30');
-	    label.textContent = stave_props.label;
-	    svg.appendChild(label);	   
+	const stave_measure_0 = new VF.Stave(10, 0, 280);
+	stave_measure_0
+	    .addClef("treble")
+	    .addTimeSignature(stave_props.timesignature.upper + "/" + stave_props.timesignature.upper)
+	    .setContext(context)
+	    .draw();
+	
+	let notes_measure_0 = measures.shift();
+		
+	// Helper function to justify and draw a 4/4 voice
+	VF.Formatter.FormatAndDraw(context, stave_measure_0, notes_measure_0);
+
+	let width = stave_measure_0.width + stave_measure_0.x;
+
+	
+	
+	for (const [n, notes_measure] of Object.entries(measures)) {
+	    const stave_measure = new VF.Stave(width, 0, 240);
+	    stave_measure.setContext(context).draw();
+	    VF.Formatter.FormatAndDraw(context, stave_measure, notes_measure);
+	    width += stave_measure.width;
 	}
-				
-	v_idx = v_idx + 1;
-	
-	// Add a clef and time signature.
-	stave.addClef(stave_props.clef).setContext(context).draw();
-			
-	var stave_name = new VF.TextNote({
-            text: name,
-            font: {
-		family: "Mononoki",
-		size: 12,
-		weight: "bold"
-            },
-            duration: 'w'               
-	})
-	    .setLine(2)
-	    .setStave(stave)
-	    .setJustification(VF.TextNote.Justification.LEFT);
-	
-	var dyn = new VF.TextNote({
-            glyph: stave_props.dyn,
-            font: {
-		family: "Mononoki",
-		size: 12,
-		weight: "bold"
-            },
-            duration: 'w'               
-	})
-	    .setLine(2)
-	    .setStave(stave)
-	    .setJustification(VF.TextNote.Justification.LEFT);
-	
-	
-	// Connect it to the rendering context and draw!
-	stave.setContext(context).draw();
-	
-	// Create a voice in 4/4 and add above notes
-	var voice = new VF.Voice({num_beats: stave_props.notes.length,  beat_value: 1}).setStrict(false);
-	var decorations = new VF.Voice({num_beats: 3,  beat_value: 1}).setStrict(false);
-	
-	voice.addTickables(stave_props.notes);
-	decorations.addTickables([stave_name, dyn]);
-	
-	var formatter = new VF.Formatter().joinVoices([voice,decorations]).format([voice,decorations], stave_props.notes.length * spacing);
 
-	// Render voice
-	voice.draw(context, stave);
-	stave_name.setContext(context).draw();
-	dyn.setContext(context).draw();	
-    }
-
-
-    // render textfields
-    for (const [name, textfield_props] of Object.entries(textfields)) {
-	var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-	text.setAttributeNS(null, 'x', textfield_props.x);
-	text.setAttributeNS(null, 'y', textfield_props.y);
-	text.setAttributeNS(null, 'fill', '#000');
-	text.textContent = textfield_props.content;
-	svg.appendChild(text);
+	
     }
 }
 
@@ -148,7 +135,6 @@ oscPort.open();
 oscPort.on("message", function (msg) {
     
     switch(msg.address) {
-
 
     case "/voice/markcurrent": {
 	// whether the current note should be marked
@@ -253,6 +239,7 @@ oscPort.on("message", function (msg) {
 	    staves[stave].dyn = "p";
 	}
 
+	// default position
 	if (staves[stave].x === undefined) {
 	    staves[stave].x = 10;
 	}
@@ -264,12 +251,19 @@ oscPort.on("message", function (msg) {
 	if (staves[stave].markcurrent === undefined) {
 	    staves[stave].markcurrent = Boolean(false);
 	}
+
+	// default is 4/4
+	if (staves[stave].timesignature === undefined) {
+	    staves[stave].timesignature = {};
+	    staves[stave].timesignature.upper = 4;
+	    staves[stave].timesignature.lower = 4;
+	}
      
 	staves[stave].notes.push(new VF.StaveNote({ keys: [note], duration: dur, clef: staves[stave].clef }));
 	
-	if (staves[stave].notes.length > 3) {
-	    staves[stave].notes.shift();
-	}
+	//if (staves[stave].notes.length > 3) {
+	//    staves[stave].notes.shift();
+	//}
 	
 	if (staves[stave].markcurrent) {
 	    // set the current note to a different color ...
